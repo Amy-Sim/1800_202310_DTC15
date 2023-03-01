6e6c5a53f665ebdfefaef0c76af5f40336d57718
Corey-McTavish <coreymct@gmail.com>
2023-02-28 21:08:38 -0800

    Add feature to change navbar when user is logged in or not

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -2,9 +2,19 @@
 // This function loads the parts of your skeleton 
 // (navbar, footer, and other things) into html doc. 
 //---------------------------------------------------
-function loadSkeleton(){
-    console.log($('#navbarPlaceholder').load('./text/nav.html'));
-    // (corey, commented out for now since we don't have a footer)
-    // console.log($('#footerPlaceholder').load('./text/footer.html'));
+function loadSkeleton() {
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // User is signed in.
+            // Do something for the user here.
+            console.log($('#navbarPlaceholder').load('./text/nav_after_login.html'));
+            // console.log($('#footerPlaceholder').load('./text/footer.html'));
+        } else {
+            // No user is signed in.
+            console.log($('#navbarPlaceholder').load('./text/nav_before_login.html'));
+            // console.log($('#footerPlaceholder').load('./text/footer.html'));
+        }
+    });
 }
-loadSkeleton();  //invoke the function
+loadSkeleton(); //invoke the function
