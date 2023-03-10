ef2f1dc6bd668f2338c0545f1a1f7ae806eaf2fa
Corey-McTavish <coreymct@gmail.com>
2023-03-10 11:22:45 -0800

    modify buddypreferences.js to display the correct switch state when the page loads

--- scripts/buddy_preferences.js
+++ scripts/buddy_preferences.js
@@ -1,14 +1,32 @@
-document.getElementById("saveBuddyPreferences").addEventListener("click", function() {
-    var user = firebase.auth().currentUser;
-    var buddyPreferences = {
-        Department: document.getElementById("departmentSwitch").checked,
-        Gender: document.getElementById("genderSwitch").checked,
-    }
-    db.collection("users").doc(user.uid).update({
-        buddyPreferences: buddyPreferences,
-    }, {merge: true}).then(function() {
-        console.log("Buddy preferences saved");
-    }).catch(function(error) {
-        console.log("Error saving buddy preferences: " + error);
+$(document).ready(function () {
+    // get the current user buddy preferences and change the switch button to the correct state
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            db.collection("users").doc(user.uid).get().then(function(doc) {
+                if (doc.exists) {
+                    var buddyPreferences = doc.data().buddyPreferences;
+                    if (buddyPreferences.Department == true) {
+                        document.getElementById("departmentSwitch").checked = true;
+                    }
+                    if (buddyPreferences.Gender == true) {
+                        document.getElementById("genderSwitch").checked = true;
+                    }
+                }
+            });
+        }
+    });
+    document.getElementById("saveBuddyPreferences").addEventListener("click", function() {
+        var user = firebase.auth().currentUser;
+        var buddyPreferences = {
+            Department: document.getElementById("departmentSwitch").checked,
+            Gender: document.getElementById("genderSwitch").checked,
+        }
+        db.collection("users").doc(user.uid).update({
+            buddyPreferences: buddyPreferences,
+        }, {merge: true}).then(function() {
+            console.log("Buddy preferences saved");
+        }).catch(function(error) {
+            console.log("Error saving buddy preferences: " + error);
+        });
     });
 });
