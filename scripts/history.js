b1bb6f55af792cf474955097c9047011ea66ccde
Fiona Wong <fwwong01@gmail.com>
2023-03-21 20:40:56 -0700

    updating history.js

--- scripts/history.js
+++ scripts/history.js
@@ -1,20 +1,12 @@
-function buddyInfo() {
-    var database = firebase.database();
-    var userBuddy = database.ref('buddyPairing');
 
-    console.log("buddyInfo() called");
-    //get buddy name
-    if (userBuddy == null) {
-        console.log("buddyPairing is null");
-    }
-    else {
-        userBuddy.on('value', function (snapshot) {
-            console.log(snapshot.val());
-            var buddyName = snapshot.val().buddyName;
-            console.log(buddyName);
-            $("#buddy-name").text(buddyName);
-        })
-    }
+function displayPairing() {
+    var userBuddy = firebase.database().ref("buddyPairing");
+    userBuddy.on('value', function (snapshot) {
+        console.log(snapshot.val());
+        var buddyName = snapshot.val().buddyName;
+        console.log(buddyName);
+        $("#buddy-name").text(buddyName);
+    })
 }
 
-buddyInfo();
+displayPairing();
