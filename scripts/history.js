621a4987d51e31e5a1208a128fd94ddf203492ff
Fiona Wong <fwwong01@gmail.com>
2023-03-21 22:09:02 -0700

    Created buddy_profile.html and buddy_profile.js

--- scripts/history.js
+++ scripts/history.js
@@ -1,19 +1,27 @@
+// populate historical pairing data
 let page = [];
 let userID = localStorage.getItem("user");
-console.log(userID);
+console.log(userID); // test to show if user id pops up
 
 
-function displayPairing() {
- 
+function displayPairingProfile() {
+    //retrieve document id from the url 
+    let params = new URL(window.location.href); // get url 
+    let ID = params.searchParams.get("docID"); // get id from url
+
+    db.collection("buddyPairing")
+
     db.collection(collection).get().then((snapshot) => {
         console.log(snapshot.val());
         var buddyName = snapshot.val().buddyName;
         console.log(buddyName);
-        $("#buddy-name").text(buddyName);
+        $("#buddy-name").text();
         $("#buddy-department").text();
         $("#time-stamp").text();
     })
 }
 
 
-page.append(displayPairing());
+
+
+
