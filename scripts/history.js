1143c9049a15cd007566442380c1b52a4bbc72a4
Fiona Wong <fwwong01@gmail.com>
2023-03-27 23:39:34 -0700

    Updating history.js to try display on page

--- scripts/history.js
+++ scripts/history.js
@@ -1,5 +1,39 @@
-
 $(document).ready(function () {
+       firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // User is signed in.
+            // Do something for the user here.
+            currentUserUID = user.uid;
+        } else {
+            // No user is signed in.
+        }
+    });
+    db.collection("requests")
+        .get()
+        .then((allEvents) => {
+            const history = [];
+            let buddycard = document.getElementById("buddyCardTemplate").content.cloneNode(true);
+            allEvents.forEach((doc) => {
+                let recipient = doc.data().recipientId;
+                let sender = doc.data().senderId;
+                console.log(recipient, sender);
+                if (sender == currentUserUID) {
+                    history.push(recipient)                    
+                    buddycard.$(".buddy-name").innerHTML = recipient.name;
+                    buddycard.$(".buddy-department").innerHTML = recipient.department;
+                    document.getElementById("buddy-card-goes-here").appendChild(buddycard);
+                }
+                if (recipient == currentUserUID) {
+                    history.push(sender)
+                    buddycard.$(".buddy-name").innerHTML = sender.name;
+                    buddycard.$(".buddy-department").innerHTML = sender.department;
+                    document.getElementById("buddy-card-goes-here").appendChild(buddycard);
+                }
+                console.log(history);
+            });
+        });
+});
+
 
     // firebase.auth().onAuthStateChanged(user => {
     //     let currentUser = db.collection("users").doc(user.uid);
@@ -23,31 +57,3 @@ $(document).ready(function () {
     //             }
     //         });
     // });
-
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-            // User is signed in.
-            // Do something for the user here.
-            currentUserUID = user.uid;
-        } else {
-            // No user is signed in.
-        }
-    });
-    db.collection("requests")
-        .get()
-        .then((allEvents) => {
-            const history = []
-            allEvents.forEach((doc) => {
-                recipient = doc.data().recipientId;
-                sender = doc.data().senderId;
-                console.log(recipient, sender);
-                if (sender == currentUserUID) {
-                    history.push(recipient)
-                }
-                if (recipient == currentUserUID) {
-                    history.push(sender)
-                }
-                console.log(history);
-            });
-        });
-});
