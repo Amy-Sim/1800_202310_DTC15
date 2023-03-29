4e68e3fe323269e862bae8e4be22f05c626a4a5b
Corey-McTavish <coreymct@gmail.com>
2023-03-28 17:47:44 -0700

    update status to complete once a notification has been sent so each time the page is loaded, that notification shouldn't show up again anymore.

--- scripts/map.js
+++ scripts/map.js
@@ -400,10 +400,13 @@ function checkRequests() {
           // Use onSnapshot() to listen for changes in the collection
           querySnapshot.forEach((doc) => {
             if (doc.data().status === "success") {
-              const phoneNumber = doc.data().phoneNumber;
+              // const phoneNumber = doc.data().phoneNumber;
               alert(
-                `Congrats, you are paired! The phone number of the person your paired with is: ${phoneNumber}.`
+                `Congrats, you are paired! The phone number of the person your paired with will be emailed to you.`
               );
+              doc.ref.update({
+                status: "complete",
+              });
               // doc.ref.delete();
             } else if (doc.data().status === "failure") {
               alert(`Sorry, your request was declined.`);
