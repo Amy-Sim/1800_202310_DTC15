be4c4b329034d7286eada71385e2560db21673d0
Corey-McTavish <coreymct@gmail.com>
2023-04-05 20:24:14 -0700

    cleanup account profile.js

--- scripts/account_profile.js
+++ scripts/account_profile.js
@@ -23,7 +23,7 @@ function populateUserInfo() {
                     if (phoneNumber != null) {
                         document.getElementById("phoneInput").value = phoneNumber;
                     }
-
+                    // get which radio button is checked for department
                     if (userDoc.data().department == "Computer Systems Technology") {
                         document.getElementById("cstInput").checked = true;
                     } else if (userDoc.data().department == "Computer Information Technology") {
@@ -32,7 +32,7 @@ function populateUserInfo() {
                         document.getElementById("otherDepartmentInput").checked = true;
                         document.getElementById("departmentSpecify").value = userDepartment
                     }
-
+                    // get which radio button is checked for gender
                     if (userDoc.data().gender == "male") {
                         document.getElementById("maleInput").checked = true;
                     } else if (userDoc.data().gender == "female") {
@@ -41,6 +41,7 @@ function populateUserInfo() {
                         document.getElementById("othergenderInput").checked = true;
                         document.getElementById("genderSpecify").value = userGender
                     }
+                    // get which radio button is checked for user type
                     if (userDoc.data().type == "driver") {
                         document.getElementById("driverInput").checked = true;
                     } else if (userDoc.data().type == "passenger") {
@@ -64,14 +65,11 @@ function editUserInfo() {
 
 function saveUserInfo() {
     console.log("inside")
-    //enter code here
-
-    // console.log('CHECK IF IMAGE UPLOAD EXISTS', localStorage.getItem('image-upload'))
+    //enter code here to save the user info to the database
 
     //a) get user entered values
     var userName = document.getElementById("nameInput").value;
     var phoneNumber = document.getElementById("phoneInput").value;
-    // var userDepartment = document.getElementById("departmentInput").value;
     // get user type
     if (document.getElementById("driverInput").checked == true) {
         var userType = "driver";
@@ -109,99 +107,3 @@ function saveUserInfo() {
     //c) disable edit
     document.getElementById('personalInfoFields').disabled = true;
 }
-
-// Commented this out (Corey)
-
-// //
-// var ImageFile; //global variable to store the File Object reference
-
-// function chooseFileListener() {
-//   const fileInput = document.getElementById("mypic-input"); // pointer #1
-//   const image = document.getElementById("mypic-goes-here"); // pointer #2
-
-//   //attach listener to input file
-//   //when this file changes, do something
-//   fileInput.addEventListener("change", function (e) {
-//     //the change event returns a file "e.target.files[0]"
-//     // localStorage.setItem('image-upload', e.target.files[0])
-
-//     ImageFile = e.target.files[0];
-//     var blob = URL.createObjectURL(ImageFile);
-
-//     //change the DOM img element source to point to this file
-//     image.src = blob; //assign the "src" property of the "img" tag
-//   });
-// }
-// chooseFileListener();
-
-// function saveUserInfo() {
-//   firebase.auth().onAuthStateChanged(function (user) {
-//     var storageRef = storage.ref("images/" + user.uid + ".jpg");
-
-//     //Asynch call to put File Object (global variable ImageFile) onto Cloud
-//     storageRef.put(ImageFile).then(function () {
-//       console.log("Uploaded to Cloud Storage.");
-
-//       //Asynch call to get URL from Cloud
-//       storageRef.getDownloadURL().then(function (url) {
-//         // Get "url" of the uploaded file
-//         console.log("Got the download URL.");
-//         //get values from the from
-//         userName = document.getElementById("nameInput").value;
-//         // userSchool = document.getElementById("schoolInput").value;
-//         // userCity = document.getElementById("cityInput").value;
-
-//         //Asynch call to save the form fields into Firestore.
-//         db.collection("users")
-//           .doc(user.uid)
-//           .update({
-//             name: userName,
-//             // school: userSchool,
-//             // city: userCity,
-//             profilePic: url, // Save the URL into users collection
-//           })
-//           .then(function () {
-//             console.log("Added Profile Pic URL to Firestore.");
-//             console.log("Saved use profile info");
-//             document.getElementById("personalInfoFields").disabled = true;
-//           });
-//       });
-//     });
-//   });
-// }
-
-// function populateInfo() {
-//   firebase.auth().onAuthStateChanged((user) => {
-//     if (user) {
-//       // go and get the curret user info from firestore
-//       currentUser = db.collection("users").doc(user.uid);
-
-//       currentUser.get().then((userDoc) => {
-//         let userName = userDoc.data().name;
-//         let userSchool = userDoc.data().school;
-//         let userCity = userDoc.data().city;
-//         let picUrl = userDoc.data().profilePic;
-
-//         if (userName != null) {
-//           document.getElementById("nameInput").value = userName;
-//         }
-//         if (userSchool != null) {
-//           document.getElementById("schoolInput").value = userSchool;
-//         }
-//         if (userCity != null) {
-//           console.log(userCity);
-//           document.getElementById("cityInput").value = userCity;
-//         }
-//         if (picUrl != null) {
-//           console.log(picUrl);
-//           // use this line if "mypicdiv" is a "div"
-//           //$("#mypicdiv").append("<img src='" + picUrl + "'>")
-//           $("#mypic-goes-here").attr("src", picUrl);
-//         } else console.log("picURL is null");
-//       });
-//     } else {
-//       console.log("no user is logged in");
-//     }
-//   });
-// }
-// populateInfo();
