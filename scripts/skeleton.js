ea5b6d486e6bf6cefb7b3ecce7fe42c0232592ce
Corey-McTavish <coreymct@gmail.com>
2023-04-06 20:02:46 -0700

    reroute html pages to be in a folder

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -8,12 +8,12 @@ function loadSkeleton() {
         if (user) {
             // User is signed in.
             // Do something for the user here.
-            console.log($('#navbarPlaceholder').load('./text/nav_after_login.html'));
-            console.log($('#footerPlaceholder').load('./text/footer.html'));
+            console.log($('#navbarPlaceholder').load('../text/nav_after_login.html'));
+            console.log($('#footerPlaceholder').load('../text/footer.html'));
         } else {
             // No user is signed in.
-            console.log($('#navbarPlaceholder').load('./text/nav_before_login.html'));
-            console.log($('#footerPlaceholder').load('./text/footer.html'));
+            console.log($('#navbarPlaceholder').load('../text/nav_before_login.html'));
+            console.log($('#footerPlaceholder').load('../text/footer.html'));
         }
     });
 }
